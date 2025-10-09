283740d004f0ecf2dfc8b4ef3afa4a98c0288950
ianpurifying <sacredmind2002@gmail.com>
2025-10-09 15:21:06 +0800

    fix the tailwind configuration

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'JobFinder')</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+
 </head>
 <body>
     <nav>
